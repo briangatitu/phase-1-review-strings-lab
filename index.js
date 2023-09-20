3fa695e7d11a8a39849bc88ba32a03f418f94d0d
BrianMutheeGatitu <brian.gatitu@student.moringaschool.com>
2023-09-20 12:50:27 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Mihari';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let  excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
